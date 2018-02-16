b178c07c0148f54520a34c17c6ed83602b086d1d
Rodolfo Ovalles <rovalles0100@gmail.com>
2018-02-15 22:55:55 -0600

    looks ws works. now adding up

---
+++ Formula/up.rb
@@ -0,0 +1,11 @@
+class Up < Formula
+  desc "Quickly navigate to a parent directory via tab-completion."
+  homepage "https://github.com/shannonmoeller/up"
+  url "https://api.github.com/repos/shannonmoeller/up/zipball/master"
+  sha256 "0d4250e3503fbf22e65e5cb17affa72a9a7e1692974913a738341a0c743f2bee"
+  version "1"
+
+  def install
+    bin.install "up.sh"
+  end
+end
