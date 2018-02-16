ea0a1c47aab80b0906078c519937c43c808b9ff1
Rodolfo Ovalles <rovalles0100@gmail.com>
2018-02-15 21:58:05 -0600

    i dont know

--- Formula/ws.rb
+++ Formula/ws.rb
@@ -3,7 +3,6 @@ class Ws < Formula
   homepage "https://github.com/rovalles/ws"
   url "https://github.com/rovalles/ws/tarball/develop"
   version "2"
-  sha256 "abdb45eea213f3938a6452cf6736853a81546137"
   def install
     bin.install "ws"
   end
