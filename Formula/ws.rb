a6f24ab9cee639c8a35913c47a8e7a580972bf5c
Rodolfo Ovalles <rovalles0100@gmail.com>
2018-02-15 22:26:36 -0600

    i still dont know fml

--- Formula/ws.rb
+++ Formula/ws.rb
@@ -1,11 +1,9 @@
 class Ws < Formula
   desc "Tab complete projects in your workspace"
   homepage "https://github.com/rovalles/ws"
-  devel do
-    url "https://github.com/rovalles/ws/tarball/develop"
+  url "https://github.com/rovalles/ws/tarball/develop"
   # url "https://github.com/rovalles/ws/tarball/develop"
-    sha256 "f486b71dbff381ad5a0ce58d430636b6b0e176669f9e517f8f2078b148fb576f"
-  end
+  sha256 "f486b71dbff381ad5a0ce58d430636b6b0e176669f9e517f8f2078b148fb576f"
   # version "2"
   # def install
   #    source "#{opt_prefix}/ws.sh
