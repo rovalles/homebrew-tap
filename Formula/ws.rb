17737cff32e0e88068c8e2806f197a247321723a
Rodolfo Ovalles <rovalles0100@gmail.com>
2018-02-15 22:36:39 -0600

    try agian

--- Formula/ws.rb
+++ Formula/ws.rb
@@ -5,9 +5,9 @@ class Ws < Formula
   # url "https://github.com/rovalles/ws/tarball/develop"
   sha256 "f486b71dbff381ad5a0ce58d430636b6b0e176669f9e517f8f2078b148fb576f"
   # version "2"
-  # def install
-  #    source "#{opt_prefix}/ws.sh
-  # end
+  def install
+    bin.install "ws.sh"
+  end
   test do
     ws -h
   end
