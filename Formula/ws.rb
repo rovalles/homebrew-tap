a6021bc0ca3de359e05b67f397d7a3f20810571c
Rodolfo Ovalles <rovalles0100@gmail.com>
2018-02-15 21:50:43 -0600

    repaired ws

--- Formula/ws.rb
+++ Formula/ws.rb
@@ -1,7 +1,7 @@
 class Ws < Formula
   desc "Tab complete projects in your workspace"
   homepage "https://github.com/rovalles/ws"
-  url "https://github.com/rovalles/ws/tar/develop"
+  url "https://github.com/rovalles/ws/tarball/develop"
   version "2"
   sha256 "abdb45eea213f3938a6452cf6736853a81546137"
   def install
