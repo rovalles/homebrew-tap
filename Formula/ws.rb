fe374061bf7b788312ee2c331feb705483e533a2
Rodolfo Ovalles <rovalles0100@gmail.com>
2018-02-15 21:35:52 -0600

    no idea what im doing

---
+++ Formula/ws.rb
@@ -0,0 +1,13 @@
+class Foobar < Formula
+  desc "Tab complete projects in your workspace"
+  homepage "https://github.com/rovalles/ws"
+  url "https://github.com/rovalles/ws/tar/develop"
+  version "2"
+  sha256 "abdb45eea213f3938a6452cf6736853a81546137"
+  def install
+    bin.install "ws"
+  end
+  test do
+    ws -h
+  end
+end
