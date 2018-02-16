d3cbe407b0dc41baf566292147a03909b9d98c3f
Rodolfo Ovalles <rovalles0100@gmail.com>
2018-02-15 22:42:22 -0600

    try agian kldsjfklsjkfl

--- Formula/ws.rb
+++ Formula/ws.rb
@@ -2,13 +2,9 @@ class Ws < Formula
   desc "Tab complete projects in your workspace"
   homepage "https://github.com/rovalles/ws"
   url "https://github.com/rovalles/ws/tarball/develop"
-  # url "https://github.com/rovalles/ws/tarball/develop"
   sha256 "f486b71dbff381ad5a0ce58d430636b6b0e176669f9e517f8f2078b148fb576f"
-  # version "2"
+
   def install
     bin.install "ws.sh"
-  end
-  test do
-    ws -h
   end
 end
