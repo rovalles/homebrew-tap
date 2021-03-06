edaa7aaf11889b0665953a7e3e3159924a66ce6e
Rodolfo Ovalles <rodolfoovalles@rodolfos-mbp.lan>
2021-03-05 23:12:31 -0600

    damn it

--- Formula/bender.rb
+++ Formula/bender.rb
@@ -3,7 +3,7 @@ class Bender < Formula
   homepage "https://github.com/rovalles/bender"
   url "https://github.com/rovalles/bender/tarball/master"
   sha256 "32195b7f95590d1820b33f90b3ef7a47b6229dafeadb8b3fcb5bd28af27886a1"
-  version "2.0"
+  version "2.1"
 
   def install
     bin.install "bender.sh"
