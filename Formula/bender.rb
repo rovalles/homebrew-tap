6c588e468ea8918f80a019ffdd9380ea3aa1b55a
rovalles <rovalles0100@gmail.com>
2021-03-14 22:27:42 -0500

    bender update

--- Formula/bender.rb
+++ Formula/bender.rb
@@ -2,8 +2,8 @@ class Bender < Formula
   desc "My way of loading my dotfiles."
   homepage "https://github.com/rovalles/bender"
   url "https://github.com/rovalles/bender/tarball/master"
-  sha256 "32195b7f95590d1820b33f90b3ef7a47b6229dafeadb8b3fcb5bd28af27886a1"
-  version "2.3"
+  sha256 "6a0fe94ed819102fa2b169615f1d877e5dfe8be3c8e84a037339b2a4c1f22fea"
+  version "3.0"
 
   def install
     bin.install "bender.sh"
